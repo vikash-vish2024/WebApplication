0b26853edcb522e68b529dc013647d1aa401a20e
Vikash Vishwakarma <vikashv@infinite.com>
2024-04-29 11:52:56 +0530

    Assesment_1 MVC

--- Assessments/MVC/Asssesment_1_2/Asssesment_1_2/Controllers/MoviesController.cs
+++ Assessments/MVC/Asssesment_1_2/Asssesment_1_2/Controllers/MoviesController.cs
@@ -14,13 +14,13 @@ namespace Asssesment_1_2.Controllers
     {
         private MoviesContext db = new MoviesContext();
 
-        // GET: Movies
+      
         public ActionResult Index()
         {
             return View(db.movies.ToList());
         }
 
-        // GET: Movies/Details/5
+      
         public ActionResult Details(int? id)
         {
             if (id == null)
@@ -35,7 +35,6 @@ namespace Asssesment_1_2.Controllers
             return View(movies);
         }
 
-        // GET: Movies/Create
         public ActionResult Create()
         {
             return View();
@@ -54,7 +53,7 @@ namespace Asssesment_1_2.Controllers
             return View(movies);
         }
 
-        // GET: Movies/Edit/5
+        
         public ActionResult Edit(int? id)
         {
             if (id == null)
@@ -81,7 +80,7 @@ namespace Asssesment_1_2.Controllers
             return View(movies);
         }
 
-        // GET: Movies/Delete/5
+     
         public ActionResult Delete(int? id)
         {
             if (id == null)
@@ -96,9 +95,8 @@ namespace Asssesment_1_2.Controllers
             return View(movies);
         }
 
-        // POST: Movies/Delete/5
+        
         [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
         public ActionResult DeleteConfirmed(int id)
         {
             Movies movies = db.movies.Find(id);
